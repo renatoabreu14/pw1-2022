a1030f84b6816a7ebeaf3422982d44e5f394219c
renatoabreu14 <renato.abreu.info@gmail.com>
2022-11-04 19:17:29 -0300

    Aula dia: 21/10/2022

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -28,7 +28,7 @@ function setList(list){
         '                    <th>Quantidade</th>\n' +
         '                    <th>Valor</th>\n' +
         '                    <th>Subtotal</th>\n' +
-        '                    <th>-</th>\n' +
+        '                    <th class="col-2">-</th>\n' +
         '                </tr>\n' +
         '                </thead>\n'+
         '               <tbody>';
@@ -40,22 +40,99 @@ function setList(list){
             '      <td>'+list[key].amount+'</td>\n' +
             '      <td>'+formatValue(list[key].value)+'</td>\n' +
             '      <td>'+formatValue(subTotal(list[key].amount, list[key].value))+'</td>\n' +
-            '      <td>Edit | Delete</td>\n' +
+            '      <td class="col-2">' +
+            '           <button onclick="setUpdate('+key+')" class="btn btn-outline-dark">Edit</button>' +
+            '           <button onclick="deleteData('+key+')" class="btn btn-outline-danger">Delete</button>' +
+            '       </td>\n' +
             '     </tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
 }
 
+function deleteData(index){
+    if (confirm('Deseja realmente excluir esse item?')){
+        if (index === list.length - 1){
+            list.pop();
+        }else if (index === 0){
+            list.shift();
+        }else{
+            var arrIni = list.slice(0, index);
+            var arrFim = list.slice(index+1);
+            list =  arrIni.concat(arrFim);
+        }
+        setList(list);
+    }
+}
+
+function setUpdate(index){
+    var item = list[index];
+    document.getElementById("inputIdUpdate").value = index;
+    document.getElementById("description").value = item.description;
+    document.getElementById("amount").value = item.amount;
+    document.getElementById("value").value = item.value;
+    document.getElementById("btnAdd").style.display = "none";
+    document.getElementById("btnUpdate").style.display = "inline-block";
+}
+
+function resetForm(){
+    document.getElementById("description").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("inputIdUpdate").value = "";
+    document.getElementById("btnAdd").style.display = "inline-block";
+    document.getElementById("btnUpdate").style.display = "none";
+}
+
+function updateData(){
+    if (!validation()){
+        return;
+    }
+    var id = document.getElementById("inputIdUpdate").value;
+    var description = document.getElementById("description").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    list[id] = {"description":description, "amount":amount, "value":value};
+    resetForm();
+    setList(list);
+}
+
 function addData(){
+    if (!validation()){
+        return;
+    }
     var description = document.getElementById("description").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
 
     list.unshift({"description":description, "amount":amount, "value":value});
-
+    resetForm();
     setList(list);
+}
 
+function validation(){
+    var description = document.getElementById("description").value;
+    var amount = parseInt('0'+document.getElementById("amount").value);
+    var value = parseFloat('0'+document.getElementById("value").value);
+    var errors = "";
+
+    if (description === ""){
+        errors += '<p>Informe a descricão</p>';
+    }
+    if (amount <= 0){
+        errors += '<p>A quantidade deve ser maior do que zero</p>';
+    }
+    if (value <= 0){
+        errors += '<p>O valor deve ser maior do que zero</p>';
+    }
+
+    if (errors != ""){
+        document.getElementById("errors").style.display = "block";
+        document.getElementById("errors").innerHTML = '<h3>Error</h3>' + errors;
+        return 0;
+    }
+    document.getElementById("errors").style.display = "none";
+    return 1;
 }
 
 setList(list);
